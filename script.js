4f27ae47d97c36ba255d7473cf10ffaaa6b27b81
ba2-ba <112476436+ba2-ba@users.noreply.github.com>
2023-08-06 23:55:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
+	for(let k in sampleObject ){
+		   if(k===key){
+			   return true;
+		   }
+	   }
+	   return false;
 }
 
 // Do not change the code below
